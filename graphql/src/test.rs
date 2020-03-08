e30d11a9d899ee5d52bf6b92b0d30ec94029d142
Andreas Dzialocha <kontakt@andreasdzialocha.com>
2020-03-08 19:33:36 +0100

    Remove unused file

--- graphql/src/test.rs
+++
@@ -1,6 +0,0 @@
-pub fn test() {
-    let schema = Schema::parse("type User { id: ID! }").unwrap();
-
-    let mut registry: juniper::Registry = juniper::Registry::new(FnvHashMap::default());
-    let meta = Input::meta(&(), &mut registry);
-}
